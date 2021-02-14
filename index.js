96ba8612b0aabe42d2a7d9de24ba003123b3bbf5
Sarah Gu <guwen888@gmail.com>
2021-02-14 10:31:55 -0500

    new roomlist endpoint and js bug fixes

--- index.js
+++ index.js
@@ -16,8 +16,11 @@ app.use('/client', express.static(path.join(__dirname, 'client')))
 // -------------- express 'get' handlers -------------- //
 app.get('/room', function(req, res){
     console.log('no sub-page');
-    res.render('index', {})
+    res.render('index', {});
 });
+app.get('/roomlist', function(req,res){
+    res.render('roomlist', {});
+}); 
 app.get('/getcode', function(req,res){
     res.render('registration', {})
 });
@@ -79,14 +82,8 @@ io.sockets.on('connection', function(socket){
         var randCode = '' + Math.floor(Math.random() * 10000);
         ROOM[randCode] = {
             sockets: [], 
-            TABLE_LIST: [], 
-            POINTS: {}, 
             DICTIONARY: [], 
             ALREADY_SEEN: new Set(),
-            TIMER: 30,
-            teamNumber: 0,
-            currTeam: 0,
-            bool: false
         };
         socket.emit('redirect', {num: '1', code: randCode}); 
     });
@@ -99,14 +96,7 @@ io.sockets.on('connection', function(socket){
             socket = SOCKET_LIST[socket.id]; 
             if(socket.room !== undefined){
                 var code = socket.room; 
-                var divId = '';
                 var roomSockets = ROOM[code].sockets;
-                var table = ROOM[code].TABLE_LIST;
-                var tableIdx = table.indexOf(socket.id);
-                if(tableIdx != -1){
-                    divId = socket.divId;
-                    ROOM[code].TABLE_LIST.splice(tableIdx, 1);
-                }
                 var idx = roomSockets.indexOf(socket.id);
                 if(idx != -1){
                     ROOM[code].sockets.splice(idx,1);
@@ -116,19 +106,10 @@ io.sockets.on('connection', function(socket){
                     delete ROOM[code];
                 }
                 else{
-                    for(var team in ROOM[code].POINTS){
-                        var curplayers = ROOM[code].POINTS[team].players; 
-                        if(curplayers[0] == socket.id || curplayers[1] == socket.id){
-                            delete ROOM[code].POINTS[team];
-                            ROOM[code].teamNumber -=1;
-                        }
             
-                    }
                     console.log(ROOM);
                     for(var s = 0; s < ROOM[code].sockets.length; s++){
                         var socket2 = SOCKET_LIST[ROOM[code].sockets[s]];
-                        socket2.emit('clearTeams');
-                        socket2.emit('clearTable');
                         socket2.emit('clearLobby');
                     }
                 }
@@ -138,65 +119,6 @@ io.sockets.on('connection', function(socket){
         }
     });
 
-    socket.on('updateTable', function(data){
-        socket = SOCKET_LIST[socket.id];
-
-        var code = socket.room; 
-        if(!socket.inTable){
-            socket.inTable = true; 
-            socket.divId = data.divId; 
-            ROOM[code].TABLE_LIST = [];
-            var players = ROOM[code].sockets;
-            for(var s = 0; s < players.length; s++){
-                var ss = SOCKET_LIST[players[s]];
-                if(ss.inTable){
-                    ROOM[code].TABLE_LIST.push(ss.id); 
-                }
-            }
-        }
-        else{
-            var og = socket.divId; 
-            socket.divId = data.divId; 
-            var teamChange = false; 
-            for(var team in ROOM[code].POINTS){
-                var curplayers = ROOM[code].POINTS[team].players; 
-                if(curplayers[0] == socket.id || curplayers[1] == socket.id){
-                    delete ROOM[code].POINTS[team];
-                    ROOM[code].teamNumber -=1;
-                    teamChange = true; 
-                }
-        
-            }
-            
-            for(var s = 0; s < ROOM[code].sockets.length; s++){
-                var ss = SOCKET_LIST[ROOM[code].sockets[s]];
-                if(teamChange){
-                    ss.emit('clearTeams');
-                }
-                ss.emit('clearTablePos', {divId: og});
-            }
-        }
-
-        for(var n = 0; n < ROOM[code].TABLE_LIST.length; n++){
-            var i = ROOM[code].TABLE_LIST[n]; 
-            var nums = parseInt(data.divId.substring(4));
-            if(nums%2 == 0 && ('game' + (''+(nums+1))) == SOCKET_LIST[i].divId){
-                SOCKET_LIST[i].team = ROOM[code].teamNumber; 
-                SOCKET_LIST[socket.id].team = ROOM[code].teamNumber; 
-                ROOM[code].POINTS[ROOM[code].teamNumber] = {pts:0, name:ROOM[code].teamNumber, players:[socket.id, i]}; 
-                ROOM[code].teamNumber += 1; 
-                break; 
-            }
-            else if(nums%2 != 0 && ('game' + (''+(nums-1))) == SOCKET_LIST[i].divId){
-                SOCKET_LIST[i].team = ROOM[code].teamNumber; 
-                SOCKET_LIST[socket.id].team = ROOM[code].teamNumber; 
-                ROOM[code].POINTS[ROOM[code].teamNumber] = {pts:0, name:ROOM[code].teamNumber, players:[socket.id, i]}; 
-                ROOM[code].teamNumber += 1; 
-                break; 
-            }
-        }
-
-    });
     socket.on('updateid', function(data){
         socket = SOCKET_LIST[socket.id]; 
 
@@ -207,46 +129,21 @@ io.sockets.on('connection', function(socket){
                 break;
             }
         }
-        for(var team in ROOM[code].POINTS){
-            var curplayers = ROOM[code].POINTS[team].players; 
-            if(curplayers[0] == socket.id){
-                ROOM[code].POINTS[team].players = [data.newId, curplayers[1]];
-                break; 
-            }
-            else if(curplayers[1] == socket.id){
-                ROOM[code].POINTS[team].players = [curplayers[0], data.newId]; 
-            }
 
-        }
         delete SOCKET_LIST[socket.id];
         socket.id = data.newId;
         SOCKET_LIST[data.newId] = socket; 
     });
-    socket.on('updateteamid', function(data){
-        socket = SOCKET_LIST[socket.id];
 
-        var code = socket.room; 
-        if(socket.team in ROOM[code].POINTS){
-            ROOM[code].POINTS['' + socket.team].name = data.newId; 
-        }
-    });
     socket.on('reset', function(data){
         socket = SOCKET_LIST[socket.id];
         var code = socket.room; 
-        ROOM[code].TABLE_LIST = [];
-        ROOM[code].POINTS = {};
         ROOM[code].DICTIONARY = [];
-        ROOM[code].TIMER = 30;
-        ROOM[code].bool = false; 
-        ROOM[code].teamNumber = 0; 
         var players = ROOM[code].sockets;
         for(var i = 0; i < players.length; i++){
             var socket2 = SOCKET_LIST[players[i]];
-            socket2.inTable = false; 
             socket2.divId = undefined; 
-            socket2.emit('clearTeams');
             socket2.emit('clearLobby');
-            socket2.emit('clearTablePos', {divId: 'reset'});
         }
     
     });
@@ -268,8 +165,6 @@ io.sockets.on('connection', function(socket){
         var idx = ROOM[code].DICTIONARY.indexOf(data.word); 
 
         ROOM[code].ALREADY_SEEN.add(idx); 
-        if(socket.team in ROOM[code].POINTS)
-            ROOM[code].POINTS['' + socket.team].pts += 1;
         var players = ROOM[code].sockets;
         for(var i in players){
             var socket2 = SOCKET_LIST[players[i]];
@@ -307,81 +202,17 @@ io.sockets.on('connection', function(socket){
         console.log(ROOM[code].ALREADY_SEEN);
     })
 //stuff for game functionality
-    socket.on('startGame', function(data){
-        socket = SOCKET_LIST[socket.id];
-        var code = socket.room; 
-        console.log(ROOM[code].POINTS);
-
-        if((Object.keys(ROOM[code].POINTS).length * 2) == ROOM[code].TABLE_LIST.length){
-            ROOM[code].bool = true; 
-            var players = ROOM[code].sockets;
-            for(var i in players){
-                var socket2 = SOCKET_LIST[players[i]];
-                socket2.emit('clientStart', {
-                    mainP1: SOCKET_LIST[ROOM[code].POINTS[ROOM[code].currTeam].players[0]].divId,
-                    mainP2: SOCKET_LIST[ROOM[code].POINTS[ROOM[code].currTeam].players[1]].divId
-                });
-            }
-        }
-        else{
-            socket.emit('oddPlayer');
-        }
-
-    });
-//timers
-    socket.on('resumeGame', function(data){
-        socket = SOCKET_LIST[socket.id];
-        var code = socket.room;
-        ROOM[code].bool = true; 
-    });
-    socket.on('pause', function(data){
-        socket = SOCKET_LIST[socket.id];
-        var code = socket.room;
-        ROOM[code].bool = false; 
-    });
-
 
 });
-function noTime(code) {
-    ROOM[code].TIMER = 30; 
-    ROOM[code].bool = false; 
-
-    var prevTeam = ROOM[code].currTeam; 
-    ROOM[code].currTeam = (ROOM[code].currTeam + 1)%(ROOM[code].teamNumber); 
-    console.log(ROOM[code].currTeam);
-    for(var i in ROOM[code].sockets){
-        var socket2 = SOCKET_LIST[ROOM[code].sockets[i]];
-        socket2.emit('highlight', {                   
-            mainP1: SOCKET_LIST[ROOM[code].POINTS[ROOM[code].currTeam].players[0]].divId,
-            mainP2: SOCKET_LIST[ROOM[code].POINTS[ROOM[code].currTeam].players[1]].divId,
-            prevP1: SOCKET_LIST[ROOM[code].POINTS[prevTeam].players[0]].divId,
-            prevP2: SOCKET_LIST[ROOM[code].POINTS[prevTeam].players[1]].divId
-        });
-    }
-    
-}
-function countdown() {
-    for(var code in ROOM){
-        if (ROOM[code].TIMER === 0) {
-            noTime(code);
-        } else if(ROOM[code].bool){
-            //elem.innerHTML = timeLeft + ' seconds remaining';
-            ROOM[code].TIMER--;
-        }
-    }
-}
 //send data real-time to clientside
 setInterval(function(){
     var packLobby = {};
     for(var r in ROOM){
-        packLobby[r] = [ROOM[r].TIMER, ROOM[r].POINTS];
         for(var i = 0; i < ROOM[r].sockets.length; i++){
             if(ROOM[r].sockets[i] in SOCKET_LIST){
                 var socket2 = SOCKET_LIST[ROOM[r].sockets[i]];
                 packLobby[r].push({
                     id:socket2.id,
-                    divId: socket2.divId,
-                    inTable: socket2.inTable
                 }); //builds a pack of current users and their associated data
             }
         }
